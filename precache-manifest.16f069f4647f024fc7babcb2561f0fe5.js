f7d4eb3bbd9d0b38652f44077474644d4524f401
Aleksandar <aleksandar.naskovich@gmail.com>
2020-06-12 00:45:48 +0200

    Updates

---
+++ precache-manifest.16f069f4647f024fc7babcb2561f0fe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce0563476ea7edcda4d85f3513f0760",
+    "url": "/job-board/index.html"
+  },
+  {
+    "revision": "2553d8332ee2ca5e256d",
+    "url": "/job-board/static/css/main.112a2ac0.chunk.css"
+  },
+  {
+    "revision": "aa7071c23e226124416d",
+    "url": "/job-board/static/js/2.62509fce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-board/static/js/2.62509fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2553d8332ee2ca5e256d",
+    "url": "/job-board/static/js/main.aafea1d5.chunk.js"
+  },
+  {
+    "revision": "4f4c44d071feb3f38643",
+    "url": "/job-board/static/js/runtime-main.d6fb6e9f.js"
+  }
+]);
